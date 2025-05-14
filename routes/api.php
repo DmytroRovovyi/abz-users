91e7ea3799d4e0767a3033ce902492c81d05a46d
Hannibal <06hannibal@gmail.com>
2025-05-14 16:07:08 +0300

    abz-issue10: create register token

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,17 @@
 
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\PositionController;
+use App\Http\Controllers\Api\TokenController;
 use Illuminate\Support\Facades\Route;
 
+// User api.
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
 
+// Position api.
 Route::get('/positions', [PositionController::class, 'index']);
+
+// Token api.
+Route::get('/token', [TokenController::class, 'getToken']);
 
